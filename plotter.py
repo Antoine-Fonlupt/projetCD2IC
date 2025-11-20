b12ccb7665767cf8cd8a281456e8703779879125
Antoine Fonlupt <afonlupt-25@tp-3a107-06.enst.fr>
2025-11-20 20:45:25 +0100

    feat(last_part): Add run v3

--- plotter.py
+++ plotter.py
@@ -20,17 +20,18 @@ def loadRes(path):
                     try:
                         v = float(row[i])
                     except:
-                        v = 0
+                        v = data[index[i]][-1]
                     data[index[i]].append(v)
             header = False
         return data
 
-data = loadRes('sweepBivar.csv')
+data = loadRes('sweepBivar3.csv')
 
 
 
 def processData(data):
     res = {}
+    resTime = {}
     for k in data:
         if k[-1] == 'Y':
             split = k.split(',')
@@ -38,17 +39,20 @@ def processData(data):
             wmp5 = float(split[1].split('=')[-1].split(')')[0])
             if wmn2 not in res.keys():
                 res[wmn2] = {}
+                resTime[wmn2] = {}
             timeTraceName = k[:-1] + 'X'
 
-            time = np.array( data[timeTraceName])
+            time = np.array(data[timeTraceName])
             value = np.array(data[k])
 
-            res[wmn2][wmp5] = time[np.argmin(value < 0.7)]
-            res[wmn2][wmp5] = np.interp(50e-9, time, value)
-    return res
+            #res[wmn2][wmp5] = time[np.argmin(value < 0.7)]
+            res[wmn2][wmp5] = np.interp(140e-9, time, value) #value
+            resTime[wmn2][wmp5] = time
+
+    return res, resTime
 
 
-res = processData(data)
+res, resTime = processData(data)
 wmn2s = sorted(res.keys())
 wmp5s = sorted(res[wmn2s[0]].keys())
 arr = np.zeros((len(wmn2s), len(wmp5s)))
@@ -58,13 +62,17 @@ for i in range(len(wmn2s)):
         try:
             arr[i, j] = res[wmn2s[i]][wmp5s[j]]
         except:
-            pass
-
+            print(".")
+        
 #ax = plt.axes(projection="3d")
 #print(type(arr[0,0]))
 #(x, y) = np.meshgrid(len(arr), len(arr[0]))
 #ax.plot_surface(x, y, arr)
+
+#plt.imshow(arr)
 plt.contourf(arr)
 plt.colorbar(label='Propagation time')
-
+"""a = wmn2s[3]
+b = wmp5s[2]
+plt.plot(resTime[a][b], res[a][b])"""
 plt.show()
